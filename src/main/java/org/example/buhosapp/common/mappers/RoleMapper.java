34c3b79a9c948296c7aacbe8789c7224ff9de3e0
Jefry-P <00016623@uca.edu.sv>
2026-06-26 16:39:53 -0600

    test

--- src/main/java/org/example/buhosapp/common/mappers/RoleMapper.java
+++ src/main/java/org/example/buhosapp/common/mappers/RoleMapper.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class RoleMapper {
-    
+
     public Role toEntityCreate(CreateRoleRequest createRoleRequest) {
         return Role.builder()
                 .name(createRoleRequest.getName())
